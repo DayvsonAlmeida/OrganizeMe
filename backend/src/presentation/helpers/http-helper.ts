4b6bfede8d4509d264389141325b21f2db9d21ac
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-08 16:42:47 -0300

    feat: add http helpers

---
+++ backend/src/presentation/helpers/http-helper.ts
@@ -0,0 +1,32 @@
+import { Http } from "../protocols";
+import { ServerError } from "../errors";
+
+export const badRequest = (error: Error): Http.Response<Error> => ({
+  statusCode: 400,
+  body: error,
+});
+
+export const forbidden = (error: Error): Http.Response<Error> => ({
+  statusCode: 403,
+  body: error,
+});
+
+export const unauthorized = (error: Error): Http.Response<Error> => ({
+  statusCode: 401,
+  body: error,
+});
+
+export const serverError = (): Http.Response<ServerError> => ({
+  statusCode: 500,
+  body: new ServerError(),
+});
+
+export const ok = (data: any): Http.Response<any> => ({
+  statusCode: 200,
+  body: data,
+});
+
+export const created = (): Http.Response => ({
+  statusCode: 201,
+  body: null,
+});
