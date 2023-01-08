6d668cbe894b03bed3133f2a487d153f188b90e0
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-08 17:13:36 -0300

    test: ensure AddProject is called properly

--- backend/src/presentation/controllers/project/create-project.spec.ts
+++ backend/src/presentation/controllers/project/create-project.spec.ts
@@ -1,13 +1,36 @@
-import { MissingParamError } from "./create-project.protocols";
+import {
+  MissingParamError,
+  AddProject,
+  Project,
+  AddProjectInput,
+} from "./create-project.protocols";
 import { CreateProjectController, Request } from "./create-project.controller";
+
+const makeAddProject = (): AddProject => {
+  class AddProjecStub implements AddProject {
+    add(project: AddProjectInput): Promise<Project> {
+      const result: Project = {
+        id: "awesome-id",
+        name: "My Project",
+      };
+      return new Promise((resolve) => resolve(result));
+    }
+  }
+  return new AddProjecStub();
+};
 
 type SutTypes = {
   sut: CreateProjectController;
+  addProjectStub: AddProject;
 };
 
-const makeSut = (): SutTypes => ({
-  sut: new CreateProjectController(),
-});
+const makeSut = (): SutTypes => {
+  const addProjectStub = makeAddProject();
+  return {
+    addProjectStub,
+    sut: new CreateProjectController(addProjectStub),
+  };
+};
 
 describe("CreateProject Controller", () => {
   it("should return 400 if no body is provided", async () => {
@@ -28,5 +51,15 @@ describe("CreateProject Controller", () => {
 
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("name"));
+  });
+
+  it("should call AddProject with correct values", async () => {
+    const { sut, addProjectStub } = makeSut();
+    const addSpy = jest.spyOn(addProjectStub, "add");
+    const httpRequest = { body: { name: "My Project" } };
+
+    await sut.handle(httpRequest);
+
+    expect(addSpy).toHaveBeenCalledWith({ name: "My Project" });
   });
 });
