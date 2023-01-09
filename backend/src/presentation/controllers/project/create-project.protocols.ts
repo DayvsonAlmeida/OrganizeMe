bcb7b55f309b74907fb9c403afee0f2459860b9e
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-08 22:31:37 -0300

    refact: move create project files to create folder

--- backend/src/presentation/controllers/project/create-project.protocols.ts
+++
@@ -1,8 +0,0 @@
-export { badRequest, serverError, ok } from "../../helpers";
-export { MissingParamError, ServerError } from "../../errors";
-export { Controller, Http } from "../../protocols";
-export { Project } from "../../../domain/entities/project";
-export {
-  AddProject,
-  AddProjectInput,
-} from "../../../domain/usecases/project/add-project";
