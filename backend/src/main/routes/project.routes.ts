f4c7c73e5a46ef6a76045e295fa55ff5e550b650
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 17:07:29 -0300

    feat: add delete projects route

--- backend/src/main/routes/project.routes.ts
+++ backend/src/main/routes/project.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { expressRouteAdapter } from "../adapters/express-route-adapter";
 import { makeCreateProjectController } from "../factories/projects/create-project-factory";
+import { makeDeleteProjectController } from "../factories/projects/delete-project-factory";
 
 export function setupProjectRoutes(router: Router): void {
   router.post("/projects", expressRouteAdapter(makeCreateProjectController()));
+  router.delete(
+    "/projects/:id",
+    expressRouteAdapter(makeDeleteProjectController())
+  );
 }
