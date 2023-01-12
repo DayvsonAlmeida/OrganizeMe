9af8c550f26d54b1e87f0a1618a275ff6ac67655
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 18:39:53 -0300

    feat: implement FindProjectRepository

--- backend/src/infra/db/in-memory/project-repository/project-in-memory-repository-adapter.ts
+++ backend/src/infra/db/in-memory/project-repository/project-in-memory-repository-adapter.ts
@@ -3,11 +3,16 @@ import { AddProjectInput } from "@/domain/usecases/project/add-project";
 import { DeleteProjectInput } from "@/domain/usecases/project/delete-project";
 import { AddProjectRepository } from "@/data/project/protocols/add-project.repository";
 import { DeleteProjectRepository } from "@/data/project/protocols/delete-project.repository";
+import { FindProjectRepository } from "@/data/project/protocols/find-project.repository";
 
 import { db, ProjectInMemoryDB } from "../db";
+import { FindProjectInput } from "@/domain/usecases/project/find-project";
 
 export class ProjectInMemoryRepositoryAdapter
-  implements AddProjectRepository, DeleteProjectRepository
+  implements
+    AddProjectRepository,
+    DeleteProjectRepository,
+    FindProjectRepository
 {
   async add(project: AddProjectInput): Promise<Project> {
     const inMemoryProject: ProjectInMemoryDB = {
@@ -31,5 +36,17 @@ export class ProjectInMemoryRepositoryAdapter
     if (!hasProject) throw new Error();
 
     delete db[idx];
+  }
+
+  async find(project: FindProjectInput): Promise<Project | null> {
+    const idx = parseInt(project.id) - 1;
+    const inMemoryProject = db.at(idx);
+
+    if (!inMemoryProject) return null;
+
+    return {
+      id: project.id,
+      name: inMemoryProject.name,
+    };
   }
 }
