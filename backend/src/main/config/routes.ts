13deb663fdc4729ff25da1fabd95a560e0243401
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 18:42:08 -0300

    feat: add create tasks route

--- backend/src/main/config/routes.ts
+++ backend/src/main/config/routes.ts
@@ -1,9 +1,11 @@
 import { Express, Router } from "express";
 import { setupProjectRoutes } from "../routes/project.routes";
+import { setupTasksRoutes } from "../routes/tasks.routes";
 
 export function setupRoutes(app: Express): void {
   const router = Router();
 
   app.use("/api", router);
   setupProjectRoutes(router);
+  setupTasksRoutes(router);
 }
