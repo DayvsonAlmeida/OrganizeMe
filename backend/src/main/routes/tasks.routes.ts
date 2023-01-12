38a1b6f1d4e79f365d779894a5c7b009fc23b620
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 20:17:31 -0300

    feat: add delete task rout

--- backend/src/main/routes/tasks.routes.ts
+++ backend/src/main/routes/tasks.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { expressRouteAdapter } from "../adapters/express-route-adapter";
 import { makeCreateTaskController } from "../factories/tasks/create-tasks-factory";
+import { makeDeleteTaskController } from "../factories/tasks/delete-tasks-factory";
 
 export function setupTasksRoutes(router: Router): void {
   router.post("/tasks", expressRouteAdapter(makeCreateTaskController()));
+  router.delete("/tasks/:id", expressRouteAdapter(makeDeleteTaskController()));
 }
