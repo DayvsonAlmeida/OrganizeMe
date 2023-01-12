298352183f7ea778d6c0bd70a506c0017d964339
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 16:41:52 -0300

    feat: add expressRouteAdapter

---
+++ backend/src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+import { Controller, Http } from "@/presentation/protocols";
+
+export function expressRouteAdapter(controller: Controller) {
+  return async (req: Request, res: Response) => {
+    const httpRequest: Http.Request = { body: req.body };
+
+    const { body, statusCode } = await controller.handle(httpRequest);
+
+    if (statusCode !== 200)
+      return res.status(statusCode).json({ message: body.message });
+
+    return res.status(statusCode).json(body);
+  };
+}
