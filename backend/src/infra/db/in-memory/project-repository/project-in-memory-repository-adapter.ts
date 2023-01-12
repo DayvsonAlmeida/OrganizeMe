f309b801525f3f124778d63ea34211abb4852e1f
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 17:02:41 -0300

    feat: implement DeleteProjectRepository

--- backend/src/infra/db/in-memory/project-repository/project-in-memory-repository-adapter.ts
+++ backend/src/infra/db/in-memory/project-repository/project-in-memory-repository-adapter.ts
@@ -1,10 +1,14 @@
 import { Project } from "@/domain/entities/project";
 import { AddProjectInput } from "@/domain/usecases/project/add-project";
+import { DeleteProjectInput } from "@/domain/usecases/project/delete-project";
 import { AddProjectRepository } from "@/data/project/protocols/add-project.repository";
+import { DeleteProjectRepository } from "@/data/project/protocols/delete-project.repository";
 
 import { db, ProjectInMemoryDB } from "../db";
 
-export class ProjectInMemoryRepositoryAdapter implements AddProjectRepository {
+export class ProjectInMemoryRepositoryAdapter
+  implements AddProjectRepository, DeleteProjectRepository
+{
   async add(project: AddProjectInput): Promise<Project> {
     const inMemoryProject: ProjectInMemoryDB = {
       name: project.name,
@@ -18,5 +22,12 @@ export class ProjectInMemoryRepositoryAdapter implements AddProjectRepository {
       id,
       name: project.name,
     };
+  }
+
+  async delete(project: DeleteProjectInput): Promise<void> {
+    const idx = parseInt(project.id) - 1;
+    const hasProject = db[idx];
+
+    if (!hasProject) throw new Error();
   }
 }
