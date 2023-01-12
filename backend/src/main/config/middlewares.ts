6f23c03cbf9af64f14bc737030229eb5094322dd
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 16:43:12 -0300

    feat: add setupMiddlewares

---
+++ backend/src/main/config/middlewares.ts
@@ -0,0 +1,11 @@
+import { Express } from "express";
+
+import { cors } from "../middlewares/cors";
+import { bodyParser } from "../middlewares/body-parser";
+import { contentType } from "../middlewares/content-type";
+
+export function setupMiddlewares(app: Express): void {
+  app.use(bodyParser);
+  app.use(cors);
+  app.use(contentType);
+}
