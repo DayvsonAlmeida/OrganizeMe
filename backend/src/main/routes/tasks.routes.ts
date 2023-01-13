5ae2d38f18dc80ba8786a8d417d78cfd7759e600
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 21:01:34 -0300

    feat: add patch task route

--- backend/src/main/routes/tasks.routes.ts
+++ backend/src/main/routes/tasks.routes.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 import { expressRouteAdapter } from "../adapters/express-route-adapter";
 import { makeCreateTaskController } from "../factories/tasks/create-tasks-factory";
 import { makeDeleteTaskController } from "../factories/tasks/delete-tasks-factory";
+import { makeToggleTaskController } from "../factories/tasks/toggle-tasks-factory";
 
 export function setupTasksRoutes(router: Router): void {
   router.post("/tasks", expressRouteAdapter(makeCreateTaskController()));
   router.delete("/tasks/:id", expressRouteAdapter(makeDeleteTaskController()));
+  router.patch("/tasks/:id", expressRouteAdapter(makeToggleTaskController()));
 }
