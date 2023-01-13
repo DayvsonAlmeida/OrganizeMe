843cbc3058eae9b0dd0add1349ee64edaef74426
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-13 06:06:11 -0300

    feat: add list fetchProject service

---
+++ frontend/src/services/projects/list.ts
@@ -0,0 +1,28 @@
+import { api } from "../api";
+
+type ApiTask = {
+  id: string;
+  name: string;
+  responsible: string;
+  deadLine: string;
+  done: boolean;
+};
+
+type ApiProject = {
+  id: string;
+  name: string;
+  tasks: ApiTask[];
+};
+
+export async function fetchProjects() {
+  const response = await api.get<ApiProject[]>("/api/projects");
+
+  console.log(response.data);
+
+  return response.data.map((project) => ({
+    id: project.id,
+    name: project.name,
+    totalOfTasks: project.tasks.length,
+    completed: project.tasks.filter((task) => task.done).length,
+  }));
+}
