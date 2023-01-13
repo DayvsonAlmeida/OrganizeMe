1caf4109acbe907f4029f8415a9abbcf0b3170f8
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-12 22:51:05 -0300

    feat: add list projects route

--- backend/src/main/routes/project.routes.ts
+++ backend/src/main/routes/project.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { expressRouteAdapter } from "../adapters/express-route-adapter";
+import { makeListProjectController } from "../factories/projects/list-project-factory";
 import { makeCreateProjectController } from "../factories/projects/create-project-factory";
 import { makeDeleteProjectController } from "../factories/projects/delete-project-factory";
 
 export function setupProjectRoutes(router: Router): void {
+  router.get("/projects", expressRouteAdapter(makeListProjectController()));
   router.post("/projects", expressRouteAdapter(makeCreateProjectController()));
   router.delete(
     "/projects/:id",
