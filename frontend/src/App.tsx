9d2a86b9324c669859bac1d23bb21515a86c20bd
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-13 05:33:14 -0300

    feat: list project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import { Typography, FloatButton } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { ProjectCard } from "./components/ProjectCard";
+
 function App() {
+  const [projects] = useState([
+    {
+      id: 1,
+      name: "Meu Projeto",
+      totalOfTasks: 10,
+      completed: 0,
+    },
+    {
+      id: 2,
+      name: "Meu Projeto 2",
+      totalOfTasks: 10,
+      completed: 3,
+    },
+  ]);
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div style={{ padding: "32px 16px 0" }}>
+      <Typography.Title level={2}>OrganizeMe</Typography.Title>
+      {projects.map(({ id, ...project }) => (
+        <ProjectCard key={id} project={project} />
+      ))}
+      <FloatButton
+        type="primary"
+        icon={<PlusOutlined />}
+        tooltip="Novo Projeto"
+      />
     </div>
   );
 }
