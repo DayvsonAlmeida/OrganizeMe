25f46e1eab7b4d20076936fd3892fc25f82c9d12
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-13 06:06:26 -0300

    feat: fetch projects from api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Typography, FloatButton } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
+
 import { ProjectCard } from "./components/ProjectCard";
+import { fetchProjects } from "./services/projects/list";
 
 function App() {
-  const [projects] = useState([
+  const [projects, setProjects] = useState([
     {
-      id: 1,
+      id: "1",
       name: "Meu Projeto",
       totalOfTasks: 10,
       completed: 0,
     },
     {
-      id: 2,
+      id: "2",
       name: "Meu Projeto 2",
       totalOfTasks: 10,
       completed: 3,
     },
   ]);
+
+  useEffect(() => {
+    function fetch() {
+      fetchProjects().then((data) => setProjects(data));
+    }
+
+    fetch();
+  }, []);
 
   return (
     <div style={{ padding: "32px 16px 0" }}>
