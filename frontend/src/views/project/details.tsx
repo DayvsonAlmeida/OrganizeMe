bac573f8f15970c01739372420fec237249a26ce
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-13 13:33:28 -0300

    feat: add confirmation modal to delete project

--- frontend/src/views/project/details.tsx
+++ frontend/src/views/project/details.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { Card, Typography, Divider, Button, List, Modal } from "antd";
 import { CheckOutlined, DeleteOutlined, UndoOutlined } from "@ant-design/icons";
-import { Card, Typography, Divider, Button, List } from "antd";
 
 import { Project, Task } from "../../contexts/projects";
-import { useState } from "react";
 
 export function DetailsProjectPage() {
   const location = useLocation();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const { id, name, tasks } = location.state as Project;
   const totalOfTasks = tasks.length;
   const completedTasks = tasks.filter((t) => t.done).length;
@@ -15,36 +18,72 @@ export function DetailsProjectPage() {
     console.log({ task });
   };
 
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    console.log("Delete project", id);
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <Card>
-      <div style={{ display: "flex", flexDirection: "column", minWidth: 600 }}>
+    <>
+      <Card>
         <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "flex-start",
-          }}
+          style={{ display: "flex", flexDirection: "column", minWidth: 600 }}
         >
-          <Typography.Title level={4}>{name}</Typography.Title>
-          <DeleteOutlined />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "flex-start",
+            }}
+          >
+            <Typography.Title level={4}>{name}</Typography.Title>
+            <DeleteOutlined onClick={showModal} />
+          </div>
+          <Typography.Text
+            style={{}}
+          >{`${completedTasks} de ${totalOfTasks}`}</Typography.Text>
+          <Divider style={{ margin: "12px 0" }} />
+          <List
+            size="small"
+            bordered
+            dataSource={tasks}
+            renderItem={(item: Task) => (
+              <TaskItem task={item} onComplete={handleOnCompleteTask} />
+            )}
+          />
+          <Button
+            type="primary"
+            style={{ width: "fit-content", marginTop: 12 }}
+          >
+            Criar Tarefa
+          </Button>
         </div>
-        <Typography.Text
-          style={{}}
-        >{`${completedTasks} de ${totalOfTasks}`}</Typography.Text>
-        <Divider style={{ margin: "12px 0" }} />
-        <List
-          size="small"
-          bordered
-          dataSource={tasks}
-          renderItem={(item: Task) => (
-            <TaskItem task={item} onComplete={handleOnCompleteTask} />
-          )}
-        />
-        <Button type="primary" style={{ width: "fit-content", marginTop: 12 }}>
-          Criar Tarefa
-        </Button>
-      </div>
-    </Card>
+      </Card>
+
+      <Modal
+        title="Deseja excluir o projeto?"
+        centered
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Cancelar
+          </Button>,
+          <Button key="submit" type="primary" onClick={handleOk}>
+            Sim
+          </Button>,
+        ]}
+      />
+    </>
   );
 }
 
