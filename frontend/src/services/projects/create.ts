3a2ccd0035df36e8050617de1f049b7a2fe13675
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2023-01-13 12:05:38 -0300

    feat: add create project service

---
+++ frontend/src/services/projects/create.ts
@@ -0,0 +1,19 @@
+import { api } from "../api";
+import { Project } from "../../contexts/projects";
+import { ApiProject } from "./dto";
+
+type CreateProjectInput = {
+  name: string;
+};
+
+export async function createProject(
+  data: CreateProjectInput
+): Promise<Project> {
+  const { data: result } = await api.post<ApiProject>("/api/projects", data);
+
+  return {
+    id: result.id,
+    name: result.name,
+    tasks: result.tasks,
+  };
+}
